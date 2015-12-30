6d055c9693938965504ea0145153f83d61e7d04f
Gareth Smyth <ghbsmyth@gmail.com>
2015-12-30 16:29:02 +0000

    Moving error messages to be local to the gem

--- lib/journey_walker.rb
+++ lib/journey_walker.rb
@@ -2,5 +2,5 @@ require 'r18n-core'
 require 'journey_walker/version'
 require 'journey_walker/journey'
 
-R18n.default_places = './i18n/'
+R18n.default_places = File.join(File.dirname(File.expand_path(__FILE__)), 'i18n')
 R18n.set('en')
